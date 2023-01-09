2355b4277656b3f7d76bef3aae3b92882c778939
lsj <1209379421@qq.com>
2023-01-10 00:20:56 +0800

    Changes

---
+++ src/main/java/com/imnu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.imnu.git;
+
+/**
+ * @author lsj
+ * @create 2023-01-09-22:17
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!!!");
+    }
+}
